cb076bdb191cb7430bbdc58d96344c49b78d6c99
somla <edesalmos@hotmail.com>
2019-11-13 18:18:09 +0100

    SHA256Salty2 added

--- client/web/js/lib/SHA256Salty.js
+++ client/web/js/lib/SHA256Salty.js
@@ -1,12 +1,13 @@
 class SHA256Salty extends IHash {
     #saltSentence;
-        
+
     constructor(saltSentence = "My own secret sentence for salt") {
         super();
         this.#saltSentence = saltSentence;
     }
 
-    string = function(str /* :string */) {
+    string(str /* :string */) /* :string */ {
+        isInheritedFrom(str, String);
         let hashArray = this.array(str);
         let res = ""
         for(let i = 0; i< hashArray.length; ++i)
@@ -18,25 +19,44 @@ class SHA256Salty extends IHash {
         return res;
     }
     
-    array = function(str /* :string */) {
-        return this.#postSalt(sha256.array(this.#preSalt(str)));
+    array(str /* :string */) /* :number[] */ {
+        return this.postSalt(sha256.array(this.preSalt(str)));
     }
 
-    #preSalt = function(str /* :string */) {
+    preSalt(str /* :string */) {
         let res = "";
         for(let i = 0; i < str.length; ++i)
         {
-            let saltChar = this.#saltSentence[i % this.#saltSentence.length];
-            res += String.fromCharCode((str[i].charCodeAt() + i * saltChar.charCodeAt() ) % 256) 
+            let saltChar = this.#saltSentence[i % this.#saltSentence.length] || "\0"; // incase if this.saltSentence is empty
+            res += String.fromCharCode((str.charCodeAt(i) + i * saltChar.charCodeAt(0) ) % 256) 
             res += saltChar;
         }
         return res;
     }
     
-    #postSalt = function(hashArray /* :Array<0..255> */) {
+    postSalt(hashArray /* :Array<0..255> */) /* :Array<0..255> */ {
         //TODO: salt algorithm
         // I'm not sure, if I make a post salt algorithm, it will decrease the
         // SHA crash or not, so I don't do that
         return hashArray;
     }
 } // end of class SHA256Salty extends HashInterface
+
+// TODO better name
+class SHA256Salty2 extends SHA256Salty
+{
+    constructor() {
+        super();
+    }
+    
+    preSalt(str /* :string */) /* :string */ {
+        let res = "";
+        for(let i = 0; i < str.length; ++i)
+        {
+            let saltChar = str[str.length -1 - i];
+            res += String.fromCharCode((str.charCodeAt(i) + i * saltChar.charCodeAt(0) ) % 256) 
+            res += saltChar;
+        }
+        return res;
+    }
+} // end of class SHA256Salty2 extends SHA256Salty
