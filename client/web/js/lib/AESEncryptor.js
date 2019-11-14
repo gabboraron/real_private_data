c4412497ed8dac3fe26cdddafd9c65879c02f5d2
somla <edesalmos@hotmail.com>
2019-11-14 12:09:01 +0100

    [m] Dependency injection

--- client/web/js/lib/AESEncryptor.js
+++ client/web/js/lib/AESEncryptor.js
@@ -2,31 +2,37 @@
 
 class AESEncryptor extends IEncryptor {
     key /* :byte[] */;
-    constructor(key) {
+    aesjs;
+    constructor(key, myAesjs = aesjs) {
         super();
         this.key = key;
+        this.aesjs = aesjs;
     }
+
     encryptFromString(data /* :string */) {
-        let aesCtr = new aesjs.ModeOfOperation.ctr(this.key);
-        let textBytes = aesjs.utils.utf8.toBytes(data);
+        let aesCtr = new this.aesjs.ModeOfOperation.ctr(this.key);
+        let textBytes = this.aesjs.utils.utf8.toBytes(data);
         let encryptedBytes = aesCtr.encrypt(textBytes);
         return encryptedBytes;
     }
+    
     descryptToString(data /* :string */) {
-        let aesCtr = new aesjs.ModeOfOperation.ctr(this.key);
+        let aesCtr = new this.aesjs.ModeOfOperation.ctr(this.key);
         let decryptedBytes = aesCtr.decrypt(data);
         return aesCtr.units.fromBytes();
     }
+    
     encryptFromNumber(data /* :Number */) {
-        let aesCtr = new aesjs.ModeOfOperation.ctr(this.key);
+        let aesCtr = new this.aesjs.ModeOfOperation.ctr(this.key);
         //TODO better way
-        let textBytes = aesjs.utils.utf8.toBytes(data.toString());
+        let textBytes = this.aesjs.utils.utf8.toBytes(data.toString());
         let encryptedBytes = aesCtr.encrypt(textBytes);
         return encryptedBytes;
     }
+    
     descryptToNumber(data /* : */) {
-        let aesCtr = new aesjs.ModeOfOperation.ctr(this.key);
-        let textBytes = aesjs.utils.utf8.toBytes(data);
+        let aesCtr = new this.aesjs.ModeOfOperation.ctr(this.key);
+        let textBytes = this.aesjs.utils.utf8.toBytes(data);
         let encryptedBytes = aesCtr.encrypt(textBytes);
         return Number(encryptedBytes);
     }
--- client/web/js/lib/SHA256Salty.js
+++ client/web/js/lib/SHA256Salty.js
@@ -1,32 +1,32 @@
 class SHA256Salty extends IHash {
     #saltSentence;
+    #sha256;
 
-    constructor(saltSentence = "My own secret sentence for salt") {
+    constructor(saltSentence = "My own secret sentence for salt", mySha256 = sha256) {
         super();
         this.#saltSentence = saltSentence;
+        this.#sha256 = mySha256;
     }
 
     string(str /* :string */) /* :string */ {
         isInheritedFrom(str, String);
         let hashArray = this.array(str);
         let res = ""
-        for(let i = 0; i< hashArray.length; ++i)
-        {
+        for(let i = 0; i< hashArray.length; ++i) {
             if(hashArray[i] < 16)
                 res += "0";
-                res += hashArray[i].toString(16);
+            res += hashArray[i].toString(16);
         }
         return res;
     }
     
     array(str /* :string */) /* :number[] */ {
-        return this.postSalt(sha256.array(this.preSalt(str)));
+        return this.postSalt(this.#sha256.array(this.preSalt(str)));
     }
 
     preSalt(str /* :string */) {
         let res = "";
-        for(let i = 0; i < str.length; ++i)
-        {
+        for(let i = 0; i < str.length; ++i) {
             let saltChar = this.#saltSentence[i % this.#saltSentence.length] || "\0"; // incase if this.saltSentence is empty
             res += String.fromCharCode((str.charCodeAt(i) + i * saltChar.charCodeAt(0) ) % 256) 
             res += saltChar;
@@ -43,16 +43,14 @@ class SHA256Salty extends IHash {
 } // end of class SHA256Salty extends HashInterface
 
 // TODO better name
-class SHA256Salty2 extends SHA256Salty
-{
-    constructor() {
-        super();
+class SHA256Salty2 extends SHA256Salty {
+    constructor(mySha256) {
+        super("", mySha256);
     }
     
     preSalt(str /* :string */) /* :string */ {
         let res = "";
-        for(let i = 0; i < str.length; ++i)
-        {
+        for(let i = 0; i < str.length; ++i) {
             let saltChar = str[str.length -1 - i];
             res += String.fromCharCode((str.charCodeAt(i) + i * saltChar.charCodeAt(0) ) % 256) 
             res += saltChar;
