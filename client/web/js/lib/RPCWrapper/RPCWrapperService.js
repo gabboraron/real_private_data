65dc4dd8d728eb395c768d216024ce79fd063ad6
somla <edesalmos@hotmail.com>
2020-01-23 15:52:15 +0100

    [m] RPC Wrapper service added

---
+++ client/web/js/lib/RPCWrapper/RPCWrapperService.js
@@ -0,0 +1,18 @@
+class RPCWrapperService {
+    constructor(){
+
+    }
+
+    async ping() {
+        return theRpcClient.call("ping");
+    }
+    async replay(message) {
+        return theRpcClient.call("message", [message]);
+    }
+    async add(x, y) {
+        return theRpcClient.call("add",[x,y]);
+    }
+    async login(userhash, passhare) {
+        return theRpcClient.call("login",[userhash, passhare]);
+    }
+}
