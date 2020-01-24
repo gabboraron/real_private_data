05ecfb1bd64394ec73b26b7091118ca02d808999
somla <edesalmos@hotmail.com>
2020-01-24 12:59:11 +0100

    [m][[python] Filemanager

--- server/python/data_manager/file_manager.py
+++ server/python/data_manager/file_manager.py
@@ -1,40 +1,46 @@
 #!/usr/bin/env python3
 
 import sys
+from config import theConfig
+
 if sys.version_info.major != 3:
     print("please use python3")
 
 import os
-import sqlite3
 
 class FileManager:
     def __init__(self,
-        root_dir: str
+        root_dir: str = theConfig.data_dir
             ):
         self.root_dir:str = root_dir
     
-    def __get_file_name(self, user_name:str, file:str = None):
-        path = self.root_dir + "/" + user_name
+    def get_file_path(self, user_dir:str, file:str = None):
+        path = self.root_dir + "/" + user_dir
         if file is not None:
             path += "/" + file
         return path
 
-    def list_dir(self, user_name: str):
-        return os.listdir(self.__get_file_name(user_name))
+    def list_dir(self, user_dir: str):
+        return os.listdir(self.get_file_path(user_dir))
     
 
-    def is_exist(self, user_name: str, file_name: str):
-        return os.path.isfile(self.__get_file_name(user_name, file_name))
+    def is_exist(self, user_dir: str, file_name: str = None):
+        if file_name is None:
+            return os.path.isdir(self.get_file_path(user_dir))
+        else:
+            return os.path.isfile(self.get_file_path(user_dir, file_name))
     
     
-    def create_file(self, 
-        user_name: str, 
+    def create_file_byte(self, 
+        user_dir: str, 
         filename: str, 
-        content: str
+        content: bytes
             ) -> bool:
         try:
-            with open(self.__get_file_name(user_name, filename), "w") as f:
+            with open(self.get_file_path(user_dir, filename), "wb") as f:
                 f.write(content)
         except Exception:
             return False
         return True
+
+    
