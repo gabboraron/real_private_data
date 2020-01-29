b53c7fb9f8bf9a103418c76f561c7a09718b558f
somla <edesalmos@hotmail.com>
2020-01-29 12:04:18 +0100

    [m][py] getclass reimplemented in python

--- client/web/html/get_class.sh
+++
@@ -1,9 +0,0 @@
-file=$1
-
-#cat $file | grep "class=" | sed -e "s/class=/\\nclass=/" | grep "class="  | sed -e 's/^class=\([^ ]*\).*/\1: \1/g'
-
-cat $file \
-   | grep "class=" \
-   | sed -e "s/class=/\\nclass=/" \
-   | grep "class="  \
-   | sed -e "s/^class=\([a-zA-Z0-9\"']*\).*/\1 : \1,/g";
---
+++ tools/getclasses.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from html.parser import HTMLParser
+import dirp
+import sys
+
+class MyHTMLParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.classes = []
+
+    def handle_starttag(self, tag, attrs):
+        for attr in attrs:
+            if attr[0] == "class":
+                self.classes = self.classes + attr[1].split(" ")
+
+
+def get_classes(f_path):
+    parser = MyHTMLParser()
+    with open(f_path, "r") as f:
+        parser.feed(f.read())
+        return parser.classes
+
+
+def get_classes_str(f_path):
+    classes = get_classes(f_path)
+    max_len = max([len(x) for x in classes])
+    rows = ""
+    for c in classes:
+        if c != "":
+            cc = '"{}"'.format(c)
+            row = '{} : {},\n'.format(cc.ljust(max_len + 2),cc)
+            rows += row
+    return rows[:-2]
+    
+if __name__ == "__main__":
+    f_path = sys.argv[1]
+    classes_str = get_classes_str(f_path)
+    print(classes_str)
