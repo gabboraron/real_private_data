b1a3a4a218aba95eb5279250b13dfe607b7c31bd
somla <edesalmos@hotmail.com>
2020-02-06 13:11:09 +0100

    [m][py] singleton documentation

--- src/python/common/singleton.py
+++ src/python/common/singleton.py
@@ -1,6 +1,22 @@
+
+"""
+This is a singleton metaclass
+usage
+MyClass(metaclass=Singleton)
+or
+MyClass(BaseClass, metaclass=Singleton)
+"""
 class Singleton(type):
+    """
+    This is a singleton metaclass
+    usage
+    MyClass(metaclass=Singleton)
+    or
+    MyClass(BaseClass, metaclass=Singleton)
+    """
     _instances = {}
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
+    
